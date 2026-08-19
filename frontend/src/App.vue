b95493708efa2e119ca56faa4ad8e5750fcf9741
tgdulecha <tinsae.dulecha@univr.it>
2026-08-19 18:22:43 +0200

    removed duplicate bootstrap

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,11 @@
 <script setup>
-// Import the external logic
-import { useApp } from "./js/App.js";
-
-// Import your components
-
 import Navbar from "./components/Navbar.vue";
-// Destructure the variables and functions for use in the template
-
 </script>
 
 <template>
   <div>
     <Navbar />
       <router-view />
-   
+
   </div>
 </template>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,7 +4,6 @@ import './css/OrderManagement.css'
 import router from './router/routers.js'
 
 
-import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 createApp(App)  
